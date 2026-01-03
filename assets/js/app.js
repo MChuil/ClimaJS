93604ba1114053ece262347f889ab252f21f2805
Miguel Chuil <developer.chuil@gmail.com>
2026-01-02 20:22:27 -0600

    Configuracion con OpenWeather

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// ========================================================
+// CONFIGURACION
+// ========================================================
+const API_KEY = 'de28b554b9d8b6b6d54c3fd6ffb4f2e7'
+const URL_BASE= 'https://api.openweathermap.org/data/3.0/onecall'
+let units = 'metric' // metric=celsius, imperial=fahrenheit
+
+
+// ========================================================
+// DOM
+// ========================================================
+const searchForm = document.querySelector('#searchForm');
+const cityInput = document.querySelector('#cityInput');
